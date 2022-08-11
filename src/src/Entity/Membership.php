3b9dfd6c9e27b33bef941677a9bdcdd44f8423f7
SahandGN <sahand.gn@gmail.com>
2022-08-11 17:55:55 +0430

    membership added to db

---
+++ src/src/Entity/Membership.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MembershipRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MembershipRepository::class)]
+#[ORM\MappedSuperclass]
+class Membership extends Product
+{
+    #[ORM\Column]
+    private ?int $length = null;
+
+    #[ORM\Column]
+    private ?int $duration = null;
+
+
+    public function getLength(): ?int
+    {
+        return $this->length;
+    }
+
+    public function setLength(int $length): self
+    {
+        $this->length = $length;
+
+        return $this;
+    }
+
+    public function getDuration(): ?int
+    {
+        return $this->duration;
+    }
+
+    public function setDuration(int $duration): self
+    {
+        $this->duration = $duration;
+
+        return $this;
+    }
+}
---
+++ src/src/Form/MembershipType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Membership;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MembershipType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('price')
+            ->add('length')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Membership::class,
+        ]);
+    }
+}
