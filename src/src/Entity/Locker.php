d7f6a3a7b6a2c4783c2d368b485f4923863f95df
SahandGN <sahand.gn@gmail.com>
2022-08-12 13:38:31 +0430

    locker added to db

---
+++ src/src/Entity/Locker.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LockerRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: LockerRepository::class)]
+class Locker
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $number = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?bool $isEmpty = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNumber(): ?int
+    {
+        return $this->number;
+    }
+
+    public function setNumber(int $number): self
+    {
+        $this->number = $number;
+
+        return $this;
+    }
+
+    public function isIsEmpty(): ?bool
+    {
+        return $this->isEmpty;
+    }
+
+    public function setIsEmpty(?bool $isEmpty): self
+    {
+        $this->isEmpty = $isEmpty;
+
+        return $this;
+    }
+}
